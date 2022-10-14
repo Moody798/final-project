ac9dcf9d4fc0859062406c9e5eddc23a6a5032c1
Mohamed Ahmed Fouad <moodymohamed798@gmail.com>
2022-10-14 17:10:20 +0200

    8th commit

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 function openMenu() {
     document.body.classList += " menu_open"
+    document.body.classList.remove('menu_close')
 }
 
 function closeMenu() {
-document.body.classList.remove('menu_open')
-
+    document.body.classList.remove('menu_open')
+    document.body.classList += " menu_close"
 }
+  
